f2875a038366f89d42610cd188fc2403bac94a19
David <delta36@gmail.com>
2019-05-17 21:48:36 +0100

    QND Initial logic for server. Handles basic requests to turn on or off light.

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ var express = require('express'),
     app = express(),
     port = process.env.PORT || 1138;
 
-//Todo: Make better and move to external config
-var authString = '/800';
-var mainlight_on = authString + '/mainlight/on';
-var mainlight_off = authString + '/mainlight/off';
+var http = require('http');
+var fs = require('fs');
+// Trying to avoid the mistake so many github developers before me have made...
+var credContents = fs.readFileSync('creds.pi', 'utf8');
+var credentials = credContents.split(/\r?\n/);
+var api_key = credentials[0];
+var controller_key = credentials[1];
+
+// Hue Bridge Routes
+//var api_root = '192.168.0.241/api/' + api_key +  '/';
+var api_root = '/api/' + api_key +  '/';
+var main_light_root = api_root + 'lights/2/';
+
+// Controller Routes
+var mainlight_on = controller_key + '/mainlight/on';
+var mainlight_off = controller_key + '/mainlight/off';
+
+
 // Scenes /mainlight/scene/[name]
 // Colours /mainlight/colour/green
 // Brightness /mainlight/brightness/up/down/number
@@ -15,12 +29,43 @@ var mainlight_off = authString + '/mainlight/off';
 //Todo: add to routing file for main light
 app.get(mainlight_on, function(req, res) {
     console.log('Light on.');
+    var requestURL = api_root + 'lights/2/state';
+
+    var options = {
+        host: '192.168.0.241',
+        port: 80,
+        path: requestURL,
+        method: 'PUT'
+    };
+
+    var reqOut = http.request(options, function(resIn) {
+        console.log("Sending");
+
+    })
+    reqOut.write('{"on":true}');
+    reqOut.end();
     //req.json();
+    res.json({message: "Done."})
 });
 
 app.get(mainlight_off, function(req, res) {
     console.log('Light off.');
-    //req.json();
+    var requestURL = api_root + 'lights/2/state';
+
+    var options = {
+        host: '192.168.0.241',
+        port: 80,
+        path: requestURL,
+        method: 'PUT'
+    };
+
+    var reqOut = http.request(options, function(resIn) {
+        console.log("Sending");
+
+    })
+    reqOut.write('{"on":false}');
+    reqOut.end();
+    res.json({message: "Done."})
 });
 
 
